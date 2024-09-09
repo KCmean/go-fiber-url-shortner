6dde3b6e9464dd4e58a8fafd82ff20b919644992
Kunal <kunalmanu2468@gmail.com>
2024-09-09 19:24:16 +0530

    fiber and routes setup

---
+++ api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+
+)
+
+
+func setupRoutes(app *fiber.App){
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1" , routes.ShortenUrl)
+}
+
+func main() {
+
+	err := godotenv.Load()
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+
+	app := fiber.New()
+
+	app.use(logger.New())
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
